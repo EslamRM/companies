660dcbdc208c9e6c806062c178e2dd2186eaf815
Eslam Ramadan <55287149+EslamRM@users.noreply.github.com>
2022-05-27 14:17:51 +0400

    some editings

--- src/components/directory/directoryCompany.vue
+++ src/components/directory/directoryCompany.vue
@@ -40,62 +40,52 @@
           <span>{{ act.title }}</span>
         </span>
         <div
-          class="text-md xl:text-sm overflow-hidden text-center sm:text-left"
+          class="xl:leading-8 leading-8 text-md xl:text-base overflow-hidden text-center sm:text-left mt-2"
         >
           <div
-            class="flex justify-start items-center mr-0 sm:mr-1 text-left mb-2"
+            class="flex justify-start items-center mr-0 sm:mr-1 text-center sm:text-left mb-2"
           >
             <!-- {{company.img}} -->
             <img
               src="../../assets/icons/phone.svg"
               class="text-gray-500 icon-size mr-1 sm:mr-2"
             />
-            <p
-              class="font-general-medium text-md md:text-base lg:text-md xl:text-md"
-            >
+            <p>
               {{ company.phone_number }}
             </p>
           </div>
           <div
-            class="flex justify-start items-center text-left mb-2 mr-0 sm:mr-1"
+            class="flex justify-start items-center text-center sm:text-left mb-2 mr-0 sm:mr-1"
           >
             <img
               src="../../assets/icons/mail.svg"
               class="text-gray-500 icon-size mr-1 sm:mr-2"
             />
-            <p
-              class="font-general-medium text-md md:text-base lg:text-md xl:text-md"
-            >
+            <p>
               {{ company.email }}
             </p>
           </div>
           <div
-            class="flex justify-start items-center mr-0 sm:mr-1 text-left mb-2"
+            class="flex justify-start items-center mr-0 sm:mr-1 text-center sm:text-left mb-2"
           >
             <img
               src="../../assets/icons/web.svg"
               class="text-gray-500 icon-size mr-1 sm:mr-2"
             />
-            <p
-              class="font-general-medium text-md md:text-base lg:text-md xl:text-md"
-            >
+            <p>
               <a target="blank" :href="company.website">{{
                 company.website
               }}</a>
             </p>
           </div>
           <div
-            class="flex justify-start items-center mr-0 sm:mr-1 text-left mb-2"
+            class="flex justify-start items-center mr-0 sm:mr-1 text-center sm:text-left mb-2"
           >
             <img
               src="../../assets/icons/location.svg"
               class="text-gray-500 icon-size mr-1 sm:mr-2"
             />
-            <p
-              class="font-general-medium text-md md:text-base lg:text-md xl:text-md"
-            >
-              {{ company.city.name }} , {{ company.area.name }}
-            </p>
+            <p>{{ company.city.name }} , {{ company.area.name }}</p>
           </div>
         </div>
       </div>
@@ -105,16 +95,16 @@
         <button
           class="col-6 w-full text-white text-sm py-2 bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 rounded-lg px-5 m-1 mb-2"
         >
-          View Website
+          <a target="blank" :href="company.website">View Website</a>
         </button>
         <button
           v-if="
             company.activePackage[0].name == 'Full' ||
             company.activePackage[0].name == 'Profile'
           "
-          class="col-6 w-full text-gray-900 text-sm py-2 bg-gradient-to-r from-lime-200 via-lime-400 to-lime-500 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-lime-300 dark:focus:ring-lime-800 rounded-lg px-5 m-1 mb-2"
+          class="col-6 w-full text-sm py-2 text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 rounded-lg px-5 m-1 mb-2"
         >
-          View Profile
+          <router-link to="projects/single-project">View Profile</router-link>
         </button>
       </div>
     </div>
